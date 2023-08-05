9076fbd6024837cb003257ebe06c92f042c4360b
German Laullon <glaullon@vmware.com>
2023-08-05 20:48:26 +0200

    map

---
+++ Makefile
@@ -0,0 +1,7 @@
+build:
+	pasmo --alocal --name pacman --tapbas main.asm pacman.tap p
+	${HOME}/Downloads/b2t80s_osx -mode 48k -tap pacman.tap --debug
+
+ 
+
+
---
+++ main.asm
@@ -0,0 +1,139 @@
+org $8000
+
+DrawMap:
+    ld hl, $4000        ;pixels 
+    ld de, $4001        ;pixels + 1
+    ld bc, $17FF        ;pixels area length - 1
+    ld (hl), 0          ;set first byte to '0'
+    ldir                ;copy bytes
+
+    ld hl, MAP
+    ld de, $5800
+
+_loop:
+    ld a, (hl)      ; next block on MAP
+    ld bc, ATTRS
+
+    add a, c        ; bc = bc + a
+    ld c, a         ;
+    adc a, b        ;
+    sub c           ;
+    ld b, a         ;
+
+    ld a, (bc)
+    ld (de), a
+    inc hl
+    inc de
+    ld a, (hl)
+    cp $FF          ; last block on MAP?
+    jp NZ, _loop
+
+DrawDots:
+    ld de, MAP
+    ld bc, 0        ; 0 x 0
+_loop:
+    ld a, (de)      ; next block on MAP
+    cp 1            ; dot ?
+    jr Z, _dot
+    cp 2                ; power dot
+    jr Z, _power
+    jr _next
+
+_dot:
+    call Get_Position_Address
+    inc h
+    inc h
+    inc h
+    ld (hl), %00011000
+    inc h
+    ld (hl), %00011000
+    jr _next
+
+_power:
+    call Get_Position_Address
+    inc h
+    inc h
+    ld (hl), %00011000
+    inc h
+    ld (hl), %00111100
+    inc h
+    ld (hl), %00111100
+    inc h
+    ld (hl), %00011000
+    jr _next
+
+_next:
+    inc c
+    ld a,c                  ; Col 32 ?
+    cp 32
+    jr NZ, _same_row
+    ld c,0
+    inc b
+_same_row:
+    inc de
+    ld a, (de)
+    cp $FF          ; last block on MAP?
+    jp NZ, _loop
+
+
+
+Fin:
+    jr Fin
+
+
+; Get Position Address 
+; b row
+; c col
+; ret hl
+; addr format 010TTSSS LLLCCCCC
+Get_Position_Address:
+    push af
+    push bc
+    ld a, b
+    rla
+    rla
+    rla
+    ld b, a
+    ld a, c
+    rla
+    rla
+    rla
+    ld c, a
+    call Get_Pixel_Address
+    pop bc
+    pop af
+    ret
+
+; Get screen address
+;  B = Y pixel position
+;  C = X pixel position
+; Returns address in HL
+;
+Get_Pixel_Address:
+    LD A,B          ; Calculate Y2,Y1,Y0
+    AND %00000111   ; Mask out unwanted bits
+    OR %01000000    ; Set base address of screen
+    LD H,A          ; Store in H
+    LD A,B          ; Calculate Y7,Y6
+    RRA             ; Shift to position
+    RRA
+    RRA
+    AND %00011000   ; Mask out unwanted bits
+    OR H            ; OR with Y2,Y1,Y0
+    LD H,A          ; Store in H
+    LD A,B          ; Calculate Y5,Y4,Y3
+    RLA             ; Shift to position
+    RLA
+    AND %11100000   ; Mask out unwanted bits
+    LD L,A          ; Store in L
+    LD A,C          ; Calculate X4,X3,X2,X1,X0
+    RRA             ; Shift into position
+    RRA
+    RRA
+    AND %00011111   ; Mask out unwanted bits
+    OR L            ; OR with Y5,Y4,Y3
+    LD L,A          ; Store in L
+    RET
+include "mapa.asm"
+
+end $8000
